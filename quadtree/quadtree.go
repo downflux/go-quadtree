4991c2be34a62815c140d18d3c2ce3dad355a8b6
Minke Zhang <minke.zhang@gmail.com>
2023-01-25 00:14:06 -0800

    Update README

--- quadtree/quadtree.go
+++ quadtree/quadtree.go
@@ -9,44 +9,6 @@ import (
 	"github.com/downflux/go-quadtree/internal/node"
 )
 
-type next struct {
-	quadrant node.Child
-	next     node.Edge
-}
-
-var (
-	// fsm is a transition table for looking up cell neighbors. See Yoder
-	// 2006 for more information. Here, R, L, D, U are replaced with EdgeE,
-	// EdgeW, EdgeS, and EdgeN respectively, and quadrants 0, 1, 2, 3 are
-	// replaced with ChildNW, ChildNE, ChildSW, and ChildSE respectively.
-	fsm = map[node.Edge]map[node.Child]next{
-		node.EdgeE: map[node.Child]next{
-			node.ChildNE: next{quadrant: node.ChildNW, next: node.EdgeE},
-			node.ChildSE: next{quadrant: node.ChildSW, next: node.EdgeE},
-			node.ChildSW: next{quadrant: node.ChildSE, next: node.EdgeNone},
-			node.ChildNW: next{quadrant: node.ChildNE, next: node.EdgeNone},
-		},
-		node.EdgeW: map[node.Child]next{
-			node.ChildNE: next{quadrant: node.ChildNW, next: node.EdgeNone},
-			node.ChildSE: next{quadrant: node.ChildSW, next: node.EdgeNone},
-			node.ChildSW: next{quadrant: node.ChildSE, next: node.EdgeW},
-			node.ChildNW: next{quadrant: node.ChildNE, next: node.EdgeW},
-		},
-		node.EdgeS: map[node.Child]next{
-			node.ChildNE: next{quadrant: node.ChildSE, next: node.EdgeNone},
-			node.ChildSE: next{quadrant: node.ChildNE, next: node.EdgeS},
-			node.ChildSW: next{quadrant: node.ChildNW, next: node.EdgeS},
-			node.ChildNW: next{quadrant: node.ChildSW, next: node.EdgeNone},
-		},
-		node.EdgeN: map[node.Child]next{
-			node.ChildNE: next{quadrant: node.ChildSE, next: node.EdgeN},
-			node.ChildSE: next{quadrant: node.ChildNE, next: node.EdgeNone},
-			node.ChildSW: next{quadrant: node.ChildNW, next: node.EdgeNone},
-			node.ChildNW: next{quadrant: node.ChildSW, next: node.EdgeN},
-		},
-	}
-)
-
 type QT struct {
 	root *node.N
 
